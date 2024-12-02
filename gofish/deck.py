1bf69678ccf89b6ec1edbd548fed6689777bae0a
Ethan Ramos <ethanramos@Ethans-MacBook-Pro-8.local>
2024-12-02 17:49:41 -0500

    Added code for the card class and deck class

---
+++ gofish/deck.py
@@ -0,0 +1,34 @@
+from card import Card
+import random
+
+
+class Deck:
+    def __init__(self, numDecks):
+        self.deck = self.buildDecks(numDecks)
+        self.discard = []
+
+    def buildDecks(self, numDecks):
+        if numDecks <= 0:
+            numDecks = 1
+        suits = ["♠", "♥", "♦", "♣"]
+        ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+
+    def draw(self):
+        if self.deck:
+            card = self.deck.pop()
+            self.discard.append(card)
+            return card
+        else:
+            print("No more cards are left")
+            return None
+
+    def shuffle(self):
+        self.deck = random.shuffle(self.deck)
+
+    def rebuildDeck(self):
+        for card in self.discard:
+            self.deck.append(card)
+        self.discard = []
